75d3073e4678397e9554393e27e069cc986658db
laurelcrelia <laurel.matt@helsinki.fi>
2023-01-30 21:25:38 +0200

    Create result template

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from os import getenv
 from flask import Flask
+from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2:///laurel"
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Testi"
+    return render_template("index.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html", sport=request.form["sport"], date=request.form["date"], comment=request.form["comment"], start=request.form["start"], end=request.form["end"],)
+
+
     
