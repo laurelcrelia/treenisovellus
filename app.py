41ad967375d5dd4e8a4717556d04112a298fb599
laurelcrelia <laurel.matt@helsinki.fi>
2023-02-02 14:52:42 +0200

    Create total time feature

--- app.py
+++ app.py
@@ -11,10 +11,21 @@ db = SQLAlchemy(app)
 @app.route("/")
 def index():
     result = db.session.execute(text("SELECT id FROM exercises"))
-    info = db.session.execute(text("SELECT type, date FROM exercises"))
+    hours = db.session.execute(text("SELECT SUM(hours) FROM exercises"))
+    minutes = db.session.execute(text("SELECT SUM(minutes) FROM exercises"))
+    info = db.session.execute(text("SELECT type, date, hours, minutes FROM exercises"))
+    total_hours = hours.fetchone()[0]
+    total_minutes=minutes.fetchone()[0]
     exercises = result.fetchall()
     information = info.fetchall()
-    return render_template("index.html", count=len(exercises), exercises=exercises, information=information)
+    return render_template("index.html", count=len(exercises), time=calculate(total_hours, total_minutes), exercises=exercises, information=information)
+
+def calculate(hours, minutes):
+    if minutes/60 >= 1:
+        hours += minutes//60
+        minutes -= 60
+    total_time = f"{hours}h {minutes}min"
+    return total_time
 
 @app.route("/form")
 def form():
@@ -23,9 +34,11 @@ def form():
 @app.route("/send", methods=["POST"])
 def send():
     type = request.form["type"]
-    date = (request.form["date"])
-    sql = text("INSERT INTO exercises (type, date) VALUES (:type, :date)")
-    db.session.execute(sql, {"type":type,"date":date})
+    date = request.form["date"]
+    hours = request.form["hours"]
+    minutes = request.form["minutes"]
+    sql = text("INSERT INTO exercises (type, date, hours, minutes) VALUES (:type, :date, :hours, :minutes)")
+    db.session.execute(sql, {"type":type,"date":date, "hours":hours, "minutes":minutes})
     db.session.commit()
     return redirect("/")
 
