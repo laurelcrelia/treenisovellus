82a230b8ba56b172b02269f2a5969cd9b71cf813
laurelcrelia <laurel.matt@helsinki.fi>
2023-02-02 12:32:53 +0200

    Improve the main page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from os import getenv
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2:///laurel"
@@ -9,15 +10,21 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT type FROM exercises"))
+    exercises = result.fetchall()
+    return render_template("index.html", count=len(exercises), exercises=exercises)
 
 @app.route("/form")
 def form():
     return render_template("form.html")
 
-@app.route("/result", methods=["POST"])
-def result():
-    return render_template("result.html", sport=request.form["sport"], date=request.form["date"], comment=request.form["comment"], start=request.form["start"], end=request.form["end"],)
+@app.route("/send", methods=["POST"])
+def send():
+    type = request.form["type"]
+    sql = text("INSERT INTO exercises (type) VALUES (:type)")
+    db.session.execute(sql, {"type":type})
+    db.session.commit()
+    return redirect("/")
 
 
     
