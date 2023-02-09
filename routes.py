a521d690c68c41327cbb731e668e5b1dbf185037
laurelcrelia <laurel.matt@helsinki.fi>
2023-02-09 17:12:47 +0200

    Improve code quality

--- routes.py
+++ routes.py
@@ -8,21 +8,27 @@ import users
 @app.route("/")
 def index():
     creator_id = users.user_id()
-    sql = text("SELECT id FROM exercises WHERE visible=1 AND creator_id=:creator_id")
-    result = db.session.execute(sql, {"creator_id":creator_id})
-    hours = db.session.execute(text("SELECT SUM(hours) FROM exercises"))
-    minutes = db.session.execute(text("SELECT SUM(minutes) FROM exercises"))
-    sql2 = text("SELECT type, date, hours, minutes FROM exercises WHERE creator_id=:creator_id ORDER BY date DESC")
-    info = db.session.execute(sql2, {"creator_id":creator_id})
-    exercises = result.fetchall()
-    if len(exercises) == 0:
+
+    sql1 = text("""SELECT COUNT(id), SUM(hours), SUM(minutes) FROM exercises WHERE visible=1 
+        AND creator_id=:creator_id""")
+    sql2 = text("""SELECT type, date, hours, minutes FROM exercises WHERE visible=1 AND creator_id=:creator_id 
+        ORDER BY date DESC""")
+
+    result1 = db.session.execute(sql1, {"creator_id":creator_id})
+    result2 = db.session.execute(sql2, {"creator_id":creator_id})
+
+    calculations = result1.fetchall()
+    exercise_information = result2.fetchall()
+
+    exercise_count = calculations[0][0]
+
+    if exercise_count == 0:
         total_hours = 0
         total_minutes = 0
     else:
-        total_hours = hours.fetchone()[0]
-        total_minutes = minutes.fetchone()[0]
-    information = info.fetchall()
-    return render_template("index.html", count=len(exercises), time=calculate(total_hours, total_minutes), exercises=exercises, information=information)
+        total_hours = calculations[0][1]
+        total_minutes = calculations[0][2]
+    return render_template("index.html", count=exercise_count, time=calculate(total_hours, total_minutes), information=exercise_information)
 
 def calculate(hours, minutes):
     if minutes/60 >= 1:
@@ -42,7 +48,8 @@ def send():
     hours = request.form["hours"]
     minutes = request.form["minutes"]
     creator_id = users.user_id()
-    sql = text("INSERT INTO exercises (type, date, hours, minutes, visible, creator_id) VALUES (:type, :date, :hours, :minutes, 1, :creator_id)")
+    sql = text("""INSERT INTO exercises (type, date, hours, minutes, visible, creator_id) 
+        VALUES (:type, :date, :hours, :minutes, 1, :creator_id)""")
     db.session.execute(sql, {"type":type,"date":date, "hours":hours, "minutes":minutes, "creator_id":creator_id})
     db.session.commit()
     return redirect("/")
